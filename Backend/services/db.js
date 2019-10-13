6d4a3bd17f9023641abccb7ffd9aa16629863f73
Daniel Amador <jdamadorsalas@gmail.com>
2019-10-13 13:10:06 -0600

    refactor: change database

--- Backend/services/db.js
+++ Backend/services/db.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 var { Pool } = require('pg');
 var isProduction = process.env.NODE_ENV === 'production';
 
-var connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
+//var connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+var connectionString = 'postgres://zgqmtjki:qkswMe4j2_OguAffRZ9aOxfYRw52hxc8@salt.db.elephantsql.com:5432/zgqmtjki';
 var pool = new Pool({
     connectionString: connectionString,
     ssl: isProduction
