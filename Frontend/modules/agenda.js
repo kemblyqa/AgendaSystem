9f95929e07d8d43d11f37e0527226d756b3b6f6d
kemblyqa <kquirosaraya@gmail.com>
2019-10-13 11:39:13 -0600

    local clase personalizada

--- Frontend/modules/agenda.js
+++ Frontend/modules/agenda.js
@@ -1,3 +1,4 @@
+import { LocalData } from '../modules/local-data.js';
 class AgendaManager {
     prueba = {
         "data": [
@@ -97,15 +98,17 @@ class AgendaManager {
             }
         ]
     }
-    constructor() { }
+    constructor() {
+        this.getAgendaList();
+     }
 
     getAgendaList() {
-
+        this.localData = new LocalData(this.prueba["data"]);
+        this.populateList(this.localData.remoteData);
     }
 
     populateList(agendaList) {
         var agendaItemsContainer = document.getElementById("agenda-list-manager");
-        this.updateLocalStorage(this.prueba.data);
 
         // Remove all items added.
         while (agendaItemsContainer.hasChildNodes()) {
@@ -118,62 +121,6 @@ class AgendaManager {
             newItem.appendChild(document.createTextNode(`Sesión N° ${this.prueba.data[i].id} \n Nombre: ${this.prueba.data[i].name}`));
             agendaItemsContainer.appendChild(newItem);
         }
-    }
-
-    /**
-     * @function updateStorage updates the items stored with the new values
-     * @param {Array} data array with the JSON from the web service 
-     */
-    updateLocalStorage(data) {
-        for (var i = 0; i < data.length; i++){
-            if (!this.existStorageItem(data[i].id) || !this.needUpdateStorageItem(data[i].id, data[i])){
-                this.setLocalStorageItem(data[i].id, data[i]);
-            }
-        }
-    }
-
-    /**
-     * @function clearLocalStorageList remove all items added
-     */
-    clearLocalStorageList() {
-        localStorage.clear();
-    }
-    /**
-     * @function setLocalStorageItem Save the new item to localstorage
-     * @param {*} key the key to access to the localstorage value
-     * @param {*} value the information about the agenda
-     */
-    setLocalStorageItem(key, value) {
-        localStorage.setItem(key, JSON.stringify(value));
-    }
-
-    /**
-     * @function getLocalStorageItem obtains the value from a key
-     * @param {string} key the key to access to the localstorage value
-     * @returns {string} the value stored
-     */
-    getLocalStorageItem(key) {
-        return localStorage.getItem(key);
-    }
-
-    /**
-     * @function existStorageItem Returns true if the item already exists
-     * @param {string} key the key to access to the localstorage value
-     * @returns {boolean} true if the value exist, false if doesn't
-     */
-    existStorageItem(key) {
-        return localStorage.getItem(key) != undefined;
-    }
-
-    /**
-     * @function needUpdateStorageItem Returns true if the items are different
-     * @param {string} key the key to access to the localstorage value
-     * @param {JSON} value the value which could be modified
-     * @returns {boolean} true if the value is different, false if are the same
-     */
-    needUpdateStorageItem(key, value) {
-        let current = localStorage.getItem(key);
-        return JSON.stringify(value) != current; //buscar mejor forma
     }
 }
 
