7fe542eba59918ce016dc04f8fe04c7bbcf51922
kemblyqa <kquirosaraya@gmail.com>
2019-10-19 10:03:14 -0600

    console info

--- Frontend/modules/ajax.js
+++ Frontend/modules/ajax.js
@@ -7,6 +7,7 @@ class Ajax {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
+                console.info("Respuesta obtenida correctamente.")
                 callBack(JSON.parse(this.responseText));
             } else if (this.status == 500){
                 console.error("Error interno del servidor.");
@@ -23,6 +24,7 @@ class Ajax {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
+                console.info("Respuesta obtenida correctamente.")
                 callBack(JSON.parse(this.responseText));
             } else if (this.status == 500){
                 console.error("Error interno del servidor.");
